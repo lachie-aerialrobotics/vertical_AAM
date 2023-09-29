74f056c84a28f6e1a9ae0b5eedbfa4d92d7cff04
lachie-aerialrobotics <lachlan.orr15@imperial.ac.uk>
2023-09-29 19:15:41 +0100

    chnging points

--- scripts/printing/trajectory_state_machine.py
+++ scripts/printing/trajectory_state_machine.py
@@ -357,8 +357,8 @@ class printStateMachine(object):
         # pose = generate_pose(0.7, 0, 0.40)
         # pose = generate_pose(0.5, 0, 0.40)
         # pose = generate_pose(0.3, 0, 0.40)
-        # pose = generate_pose(0.1, 0, 0.40)
-        pose = generate_pose(-0.1, 0, 0.40)
+        pose = generate_pose(0.1, 0, 0.40)
+        # pose = generate_pose(-0.1, 0, 0.40)
         # pose = generate_pose(-0.3, 0, 0.40)
 
         self.traj_blind.pause(pose, pause_time)
